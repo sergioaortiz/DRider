ddeff1ecfa26818dae401f265acf24e031658603
Sergio Andres Aya <sergioaortiz2302@gmail.com>
2023-03-05 03:48:48 -0500

    Migrations

---
+++ app/migrations/20220305020700_create_drivers.rb
@@ -0,0 +1,17 @@
+Sequel.migration do
+    up do
+        create_table(:drivers) do
+        primary_key :id
+        Integer :legal_id, null: false
+        String :legal_id_type, null: false
+        String :name, null: false
+        String :email, null: false
+        String :latitude, null: false
+        String :longitude, null: false
+        end
+    end
+
+    down do
+        drop_table(:drivers)
+    end
+end
---
+++ app/migrations/20220305023000_create_rides.rb
@@ -0,0 +1,19 @@
+Sequel.migration do
+    up do
+        create_table(:rides) do
+        primary_key :id
+        foreign_key :id_driver, :drivers
+        foreign_key :id_rider, :riders        
+        DateTime :starting_time, null: false
+        DateTime :final_time, null: false
+        String :starting_latitude, null: false
+        String :starting_longitude, null: false
+        String :final_latitude, null: false
+        String :final_longitude, null: false
+        end
+    end
+
+    down do
+        drop_table(:rides)
+    end
+end
